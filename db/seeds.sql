2705b8c8ba231c6da44a467c56581deff58ac02d
Sergei Velichkin <sergey.velichkin@gmail.com>
2018-05-13 22:06:27 -0600

    update

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Chicken Bacon');
+INSERT INTO burgers (burger_name) VALUES ('Double cheese');
+INSERT INTO burgers (burger_name) VALUES ('McBurger');
---
+++ models/burger.js
@@ -0,0 +1,23 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+  all: function(cb) {
+    orm.all( function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(name, cb) {
+    orm.create(name, function(res) {
+      cb(res);
+    });
+  },
+  update: function(id, cb) {
+    orm.update(id, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
